3405bbb9bec34eaa30fbdcd673cb73d682d75756
Januda Fernando <147802908+JanudaFernando@users.noreply.github.com>
2024-01-28 09:49:40 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var btn = document.querySelector('.btn');
+    btn.addEventListener('click', function() {
+        alert('Do you want to continue');
+    });
+});
